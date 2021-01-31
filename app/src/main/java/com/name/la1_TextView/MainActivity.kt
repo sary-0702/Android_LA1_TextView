f785e47cdb3247a736b8267cad8f0aeb53208ad0
sary-0702 <nya.saku.7722@gmail.com>
2021-01-31 21:20:58 +0900

    TextView

---
+++ app/src/main/java/com/name/la1_TextView/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.name.la1_TextView
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.text = "SARY"
+
+        textView.setTextColor(Color.rgb(200,50,80))
+    }
+}
